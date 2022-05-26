ea20b044bd8ee01641f0ca7eb23ad9b7b48c01d2
John Kim A. Querobines <55902501+Kerubi-5@users.noreply.github.com>
2022-05-26 20:58:41 +0800

    feat: error handling

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { HomeRouter } from "./routes";
@@ -26,6 +26,26 @@ const main = async () => {
    */
 
   app.use("/api", HomeRouter);
+  
+   /**
+   *
+   * ERROR HANDLER
+   *
+   */
+
+  app.use("*", (req: Request, res: Response, next: NextFunction) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err,
+    });
+  });
 
   app.listen(port, () => console.log(`Server started at port:${port}`));
 };
